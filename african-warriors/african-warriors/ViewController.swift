46c9c9077ea5a5d7213ebb796828560d691be133
Mohamed Hayibor <mohamedhayibor@gmail.com>
2016-09-27 19:24:25 +0000

    initial x-code8.1 and swift3 setup

---
+++ african-warriors/african-warriors/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  african-warriors
+//
+//  Created by Mohamed Hayibor on 9/27/16.
+//  Copyright © 2016 Mohamed Hayibor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
