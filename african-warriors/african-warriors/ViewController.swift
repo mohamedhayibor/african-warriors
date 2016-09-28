5106f4538205010ece9cff413be9e01d9ea2359a
Mohamed Hayibor <mohamedhayibor@gmail.com>
2016-09-28 01:38:46 +0000

    add implementation to test 15 rendered cells

--- african-warriors/african-warriors/ViewController.swift
+++ african-warriors/african-warriors/ViewController.swift
@@ -9,13 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var collection: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // example initialization
         // let lat = Warrior(name: "latdior", warriorId: 1);
+        
+        // assigning the delegate and datasource to self
+        collection.dataSource = self
+        collection.delegate = self
     }
     
+    // dequeues the cell and sets it up
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "WarriorCell", for: indexPath) as? WarriorCell {
             return cell
@@ -24,18 +31,22 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         }
     }
     
+    // executes when a cell is tapped
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         //
     }
     
+    // sets the number of items in the section
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        //
+        // return 15 for now > for testing purposes
+        return 15
     }
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
 
+    // defining the size of the cells
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: 105, height: 105)
     }
