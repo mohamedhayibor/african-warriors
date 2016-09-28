2c74ee020ba3dd0f032f8d9befb62e3b0269823d
Mohamed Hayibor <mohamedhayibor@gmail.com>
2016-09-28 01:09:34 +0000

    add Warrior cell configuration and relation to uithumb and uilabel

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ Who is this app: for history tinkerers, children, and getting ready for a test (
 I can't scour the whole web and all books. If you know of a warrior that must be included, open an issue with related links.
 
 > Built with Xcode 8.1 and Swift 3
+
+## Implementation details
+
+> The name of a warrior's image is the the string of warriorId.
---
+++ african-warriors/african-warriors/WarriorCell.swift
@@ -0,0 +1,23 @@
+//
+//  WarriorCell.swift
+//  african-warriors
+//
+//  Created by Mohamed Hayibor on 9/28/16.
+//  Copyright © 2016 Mohamed Hayibor. All rights reserved.
+//
+
+import UIKit
+
+class WarriorCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var warrior: Warrior!
+    
+    func configureCell(warrior: Warrior) {
+        self.warrior = warrior
+        nameLbl.text = self.warrior.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.warrior.warriorId)")
+    }
+    
+}
